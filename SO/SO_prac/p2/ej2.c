81b92c463c13b28b979b62523a2ff88933625801
Andres <andres.morosjr@gmail.com>
2023-12-27 12:12:09 +0100

    new-ej

--- SO/SO_prac/p2/ej2.c
+++ SO/SO_prac/p2/ej2.c
@@ -1,31 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <errno.h>
 
-int *CuentaLineas(FILE *archivo;)
+int *CuentaLineas(void* argv)
 {
-    int contador = 0;
+    int *contador = malloc(sizeof(int));
+    *contador = 0;
+
+    char *fname = (char*)argv; 
+    char aux[256];
+
     int caracter;
+    FILE* archivo=fopen(fname, "r");
 
-    while ((caracter = fgetc(archivo)) != EOF)
-    {
-        if (caracter == '\n') 
-        {
-            contador++;
-        }
+    if (archivo==NULL){                           //Si el fichero no se ha abierto correctamente, nos saltara el error
+
+        printf("Error al abrir el archivo. Codigo de error: %d\n", errno);
+        pthread_exit(NULL);                 //No ponemos exit(EXIT_FAILURE) porque si la usamos, en vez de finalizar la hebra, finalizaria el proceso (el programa)
     }
 
-    if (contador > 0 || (caracter != '\n' && caracter != EOF)) {
-        contador++;
+    while (fgets(aux, 256, archivo)!=NULL){       //Cada vez que entremos en el bucle aumentara el numero de lineas
+        ++*contador;                       //Aumentamos el numero de lineas que tiene el fichero
     }
-
-    return contador;
+    fclose(archivo);
+    printf("[HILO HIJO] El numero de lineas de este archivo = %d\n", *contador);
+    pthread_exit((void*)contador);
 }
 
 int main(int argc, char** argv)
 {
-    int num_total_lineas=0, num_hebras = argc-1;
+    if (argc < 2)
+    {
+        printf("Error debe llamar al programa de la siguiente forma: ./ej2 nombrefichero1, nombrefichero2, ..., nombreficheron\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int num_total_lineas=0, num_hebras = argc-1, *lineas_archivo;
     pthread_t hilos[num_hebras];
 
-    for
+    for(int i=0; i<num_hebras; i++)
+    {
+        if(pthread_create(&hilos[i], NULL, (void*)CuentaLineas, (void*)argv[i+1]))
+        {
+            perror("pthread_create error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    for(int i=0; i<num_hebras; i++)
+    {
+        if(pthread_join(hilos[i], (void**)&lineas_archivo))
+        {
+            perror("pthread_join error");
+            exit(EXIT_FAILURE);           
+        }
+        printf("[HILO PADRE] El valor recibido es %d\n",*lineas_archivo);
+        num_total_lineas = num_total_lineas + *lineas_archivo;
+    }
+    printf("[HILO PADRE] el numero de lineas total = %d \n", num_total_lineas);
+    exit(EXIT_SUCCESS);
 }
---
+++ SO/SO_prac/p2/ej3.c
@@ -0,0 +1,6 @@
+/*Implemente un programa que cree un vector de 10 elementos relleno con números aleatorios
+entre 1 y 9. No cree el vector de manera global, declárelo en el main(). Reparta ese vector entre 2
+hebras o 5 hebras a partes iguales, según se indique por linea de argumentos un 2 o un 5, de forma
+que cada hebra sume la parte del vector que le corresponda y se lo devuelva al hilo principal, el cual
+mostrará la suma de los resultados devuelto por las hebras creadas.
+Ejemplo de invocación del programa para crear 5 hebras que se repartan el vector: ./a.out 5*/
